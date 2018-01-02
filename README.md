ce6c19196a4eab2986c59c57cfd1d0f31aea9aa1
wangzhihao <zhihao.wang@1mifudao.com>
2018-01-02 17:56:46 +0800

    part1；hello-world

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # springboot-learn
+这是springboot学习汇总，一些demo，用于记录用springboot简化开发和完成一些小功能
+
+关于springboot用了一段时间，排除之前自己按照网上一些教学博客学习的demo，一直没有好好整理下，之前整理的其实也很杂乱，现在重新整理下。
+1. [Hello World!]()
---
+++ part01-hello-world/src/main/java/com/example/springboot/part01helloworld/Part01HelloWorldApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboot.part01helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part01HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Part01HelloWorldApplication.class, args);
+	}
+}
