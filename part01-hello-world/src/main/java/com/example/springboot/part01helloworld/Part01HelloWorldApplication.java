e8e7aa64a1afd34c055c2f6f7fc81a90f76d50bf
wangzhihao <zhihao.wang@1mifudao.com>
2018-01-03 16:46:28 +0800

    增加单元测试

--- part01-hello-world/src/main/java/com/example/springboot/part01helloworld/Part01HelloWorldApplication.java
+++ part01-hello-world/src/main/java/com/example/springboot/part01helloworld/Part01HelloWorldApplication.java
@@ -3,6 +3,11 @@ package com.example.springboot.part01helloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ *
+ * @author Administrator
+ */
 @SpringBootApplication
 public class Part01HelloWorldApplication {
 
--- part01-hello-world/src/main/java/com/example/springboot/part01helloworld/hello/SampleController.java
+++ part01-hello-world/src/main/java/com/example/springboot/part01helloworld/hello/SampleController.java
@@ -2,8 +2,11 @@ package com.example.springboot.part01helloworld.hello;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @EnableAutoConfiguration
@@ -11,7 +14,24 @@ public class SampleController {
 
     @RequestMapping("/")
     @ResponseBody
-    String home() {
+    public String home() {
         return "Hello World!";
+    }
+
+    @GetMapping("/say")
+    @ResponseBody
+    public String say(@RequestParam String content) {
+        return content;
+    }
+
+    @PostMapping("/change")
+    @ResponseBody
+    public Map<String,Object> change(@RequestBody List<String> list) {
+        Map<String,Object> resultMap =  new HashMap<>();
+        int i = 1;
+        for (String str : list ){
+            resultMap.put("part"+i++,str);
+        }
+        return resultMap;
     }
 }
