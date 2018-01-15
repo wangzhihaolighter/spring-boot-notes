97241403fb08c1d86cb0b5475d29de0394bd5c0b
wangzhihao <zhihao.wang@1mifudao.com>
2018-01-15 17:54:33 +0800

    3.3 jdbctemplate

---
+++ part03-03-jdbctemplate/src/main/java/com/example/springboot/part0303jdbctemplate/Part0303JdbctemplateApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboot.part0303jdbctemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part0303JdbctemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Part0303JdbctemplateApplication.class, args);
+	}
+}
