29dabd0eadb30fdfb8933db0169ea56966118a52
wangzhihao <zhihao.wang@1mifudao.com>
2018-01-16 10:19:04 +0800

    3.4 mybatis插件:mybatis-generator

---
+++ part03-04-mybatis-generator/src/test/java/com/example/springboot/part0304mybatisgenerator/Part0304MybatisGeneratorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springboot.part0304mybatisgenerator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Part0304MybatisGeneratorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
