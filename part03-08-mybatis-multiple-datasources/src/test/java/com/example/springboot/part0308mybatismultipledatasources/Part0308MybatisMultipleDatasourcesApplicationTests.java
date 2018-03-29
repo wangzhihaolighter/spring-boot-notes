731a09ab9175d07aef0e0566c8b255a6d857da44
wangzhihao <1984800194@qq.com>
2018-03-29 20:44:38 +0800

    nothing

--- part03-08-mybatis-multiple-datasources/src/test/java/com/example/springboot/part0308mybatismultipledatasources/Part0308MybatisMultipleDatasourcesApplicationTests.java
+++ part03-08-mybatis-multiple-datasources/src/test/java/com/example/springboot/part0308mybatismultipledatasources/Part0308MybatisMultipleDatasourcesApplicationTests.java
@@ -1,32 +1,32 @@
-package com.example.springboot.part0308mybatismultipledatasources;
-
-import com.example.springboot.part0308mybatismultipledatasources.mapper.primary.DogMapper;
-import com.example.springboot.part0308mybatismultipledatasources.mapper.primary.PeopleMapper;
-import com.example.springboot.part0308mybatismultipledatasources.mapper.second.UserMapper;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class Part0308MybatisMultipleDatasourcesApplicationTests {
-
-    @Autowired
-    private UserMapper userMapper;
-
-    @Autowired
-    private PeopleMapper peopleMapper;
-
-    @Autowired
-    private DogMapper dogMapper;
-
-    @Test
-    public void test() {
-        System.out.println(userMapper.selectByPrimaryKey(1L));
-        System.out.println(peopleMapper.selectByPrimaryKey(1L));
-        System.out.println(dogMapper.selectByPrimaryKey(1L));
-    }
-
-}
+//package com.example.springboot.part0308mybatismultipledatasources;
+//
+//import com.example.springboot.part0308mybatismultipledatasources.mapper.primary.DogMapper;
+//import com.example.springboot.part0308mybatismultipledatasources.mapper.primary.PeopleMapper;
+//import com.example.springboot.part0308mybatismultipledatasources.mapper.second.UserMapper;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class Part0308MybatisMultipleDatasourcesApplicationTests {
+//
+//    @Autowired
+//    private UserMapper userMapper;
+//
+//    @Autowired
+//    private PeopleMapper peopleMapper;
+//
+//    @Autowired
+//    private DogMapper dogMapper;
+//
+//    @Test
+//    public void test() {
+//        System.out.println(userMapper.selectByPrimaryKey(1L));
+//        System.out.println(peopleMapper.selectByPrimaryKey(1L));
+//        System.out.println(dogMapper.selectByPrimaryKey(1L));
+//    }
+//
+//}
