8b89c50c6cb0b9a7ecd5d2751d21246f8606de51
wangzhihao <zhihao.wang@1mifudao.com>
2018-04-12 15:13:48 +0800

    03-security:security demo,使用Spring Security进行基本安全控制

---
+++ 03-security/security/src/main/java/com/example/springframework/boot/security/config/SystemPasswordEncoder.java
@@ -0,0 +1,15 @@
+package com.example.springframework.boot.security.config;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class SystemPasswordEncoder implements PasswordEncoder {
+    @Override
+    public String encode(CharSequence charSequence) {
+        return charSequence.toString();
+    }
+
+    @Override
+    public boolean matches(CharSequence charSequence, String s) {
+        return s.equals(charSequence.toString());
+    }
+}
