953107dd5270acd8323bcca07123d247237c3dfb
wangzhihao <zhihao.wang@1mifudao.com>
2018-05-02 15:36:14 +0800

    03-security:使用Spring Security实现用户角色数据库管理

--- 03-security/security-database/src/main/java/com/example/springframework/boot/security/database/config/response/ResultCodeEnum.java
+++ 03-security/security-database/src/main/java/com/example/springframework/boot/security/database/config/response/ResultCodeEnum.java
@@ -21,16 +21,8 @@ public enum ResultCodeEnum {
         return code;
     }
 
-    public void setCode(int code) {
-        this.code = code;
-    }
-
     public String getMessage() {
         return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
     }
 
     ResultCodeEnum(int code, String message) {
