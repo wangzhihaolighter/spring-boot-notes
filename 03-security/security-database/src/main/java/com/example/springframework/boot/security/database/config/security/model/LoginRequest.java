db85a5c17310df806f4e14a53a6273c2e467da88
wangzhihao <zhihao.wang@1mifudao.com>
2018-05-03 10:41:12 +0800

    删除

--- 03-security/security-database/src/main/java/com/example/springframework/boot/security/database/config/security/model/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.springframework.boot.security.database.config.security.model;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String username;
-    private String password;
-}
