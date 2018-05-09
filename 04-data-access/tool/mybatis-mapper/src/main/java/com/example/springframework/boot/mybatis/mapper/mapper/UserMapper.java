060af053f10e07a1f655370e8d93254f41a2fadd
wangzhihao <zhihao.wang@1mifudao.com>
2018-05-09 16:32:25 +0800

    04-data-access: tool -> mybatis-mapper 通用mapper，简化CRUD

---
+++ 04-data-access/tool/mybatis-mapper/src/main/java/com/example/springframework/boot/mybatis/mapper/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.example.springframework.boot.mybatis.mapper.mapper;
+
+import com.example.springframework.boot.mybatis.mapper.entity.User;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserMapper extends Mapper<User> {
+}
