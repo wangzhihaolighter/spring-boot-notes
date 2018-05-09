8f5fbee60f428726a7f885a490c53ee9fb4f2ef0
wangzhihao <zhihao.wang@1mifudao.com>
2018-05-09 16:47:33 +0800

    remove

--- 04-data-access/tool/mybatis-mapper-generator/src/main/java/com/example/springframework/boot/mybatis/mapper/generator/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.springframework.boot.mybatis.mapper.generator.mapper;
-
-import com.example.springframework.boot.mybatis.mapper.generator.entity.User;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface UserMapper extends Mapper<User> {
-}
