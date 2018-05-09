02b5541b4ab3b70e75d1f12377fbd3457afe90d8
wangzhihao <zhihao.wang@1mifudao.com>
2018-05-09 10:55:39 +0800

    04-data-access: tool -> mybatis-mapper-generator

---
+++ 04-data-access/tool/mybatis-mapper-generator/src/main/java/com/example/springframework/boot/mybatis/mapper/generator/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.example.springframework.boot.mybatis.mapper.generator.mapper;
+
+import com.example.springframework.boot.mybatis.mapper.generator.entity.User;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserMapper extends Mapper<User> {
+}
