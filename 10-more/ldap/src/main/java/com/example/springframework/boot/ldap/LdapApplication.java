372e6bfdc1060309f15c87df3a55a5a4f76d56cd
wangzhihao <zhihao.wang@1mifudao.com>
2018-05-22 16:36:15 +0800

    10-more:ldap -> 使用LDAP来统一管理用户信息（LDAP:轻量级目录访问协议）

---
+++ 10-more/ldap/src/main/java/com/example/springframework/boot/ldap/LdapApplication.java
@@ -0,0 +1,12 @@
+package com.example.springframework.boot.ldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LdapApplication.class, args);
+    }
+}
