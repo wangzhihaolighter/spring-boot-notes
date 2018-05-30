b9dbb2a361185b6deef7fc68212e90805d7b1472
wangzhihao <zhihao.wang@1mifudao.com>
2018-05-30 11:26:23 +0800

    04-data-access: multiple -> data-source-aop-annotation 多数据源配置 - 方式3：AOP+注解方式,通过注解动态设置数据源

--- 04-data-access/multiple/data-source-aop-annotation/src/main/java/com/example/springframework/boot/data/source/aop/annotation/DataSourceAopAnnotationApplication.java
+++ 04-data-access/multiple/data-source-aop-annotation/src/main/java/com/example/springframework/boot/data/source/aop/annotation/DataSourceAopAnnotationApplication.java
@@ -2,7 +2,6 @@ package com.example.springframework.boot.data.source.aop.annotation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 public class DataSourceAopAnnotationApplication {
