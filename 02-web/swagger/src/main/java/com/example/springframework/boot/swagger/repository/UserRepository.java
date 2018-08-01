2f6b07f11a75478f271c2a70d8be60a75f56ab3a
wangzhihao <zhihao.wang@1mifudao.com>
2018-08-01 18:34:59 +0800

    move

--- 02-web/swagger/src/main/java/com/example/springframework/boot/swagger/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springframework.boot.swagger.repository;
-
-import com.example.springframework.boot.swagger.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
