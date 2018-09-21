289549c2186ee21fca72c62681d8adfc2835d337
wangzhihao <1984800194wzh@gmail.com>
2018-09-21 18:09:46 +0800

    code segment,代码片段

---
+++ 10-more/segment/code segment/src/main/java/com/example/springframework/boot/code/segment/CodeSegmentApplication.java
@@ -0,0 +1,12 @@
+package com.example.springframework.boot.code.segment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSegmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeSegmentApplication.class, args);
+    }
+}
