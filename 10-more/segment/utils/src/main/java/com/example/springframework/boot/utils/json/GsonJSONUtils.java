2f3c5deddfdbf302adeb91574d3ddef357e11f00
wangzhihao <1984800194wzh@gmail.com>
2018-09-29 12:02:23 +0800

    更新工具类

--- 10-more/segment/utils/src/main/java/com/example/springframework/boot/utils/json/GsonJSONUtils.java
+++ 10-more/segment/utils/src/main/java/com/example/springframework/boot/utils/json/GsonJSONUtils.java
@@ -1,8 +1,96 @@
 package com.example.springframework.boot.utils.json;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * json工具类 - Jackson实现
  */
 public class GsonJSONUtils {
-    //TODO
+    private static Gson gson;
+
+    static {
+        gson = new Gson();
+    }
+
+    private GsonJSONUtils() {
+    }
+
+    /**
+     * 对象转成json
+     *
+     * @param object 对象
+     * @return json
+     */
+    public static <T> String gsonString(T object) {
+        String gsonString = null;
+        if (gson != null) {
+            gsonString = gson.toJson(object);
+        }
+        return gsonString;
+    }
+
+    /**
+     * Json转成对象
+     *
+     * @param gsonString json字符串
+     * @param cls        转换的对象类
+     * @return 对象
+     */
+    public static <T> T gsonToBean(String gsonString, Class<T> cls) {
+        T t = null;
+        if (gson != null) {
+            t = gson.fromJson(gsonString, cls);
+        }
+        return t;
+    }
+
+    /**
+     * json转成list<T>
+     *
+     * @param gsonString json字符串
+     * @param cls        对象类
+     * @return list<T>
+     */
+    public static <T> List<T> gsonToList(String gsonString, Class<T> cls) {
+        List<T> list = null;
+        if (gson != null) {
+            list = gson.fromJson(gsonString, new TypeToken<List<T>>() {
+            }.getType());
+        }
+        return list;
+    }
+
+    /**
+     * json转成map集合
+     *
+     * @param gsonString json字符串
+     * @return List<Map<String, T>>
+     */
+    public static <T> List<Map<String, T>> gsonToListMaps(String gsonString) {
+        List<Map<String, T>> list = null;
+        if (gson != null) {
+            list = gson.fromJson(gsonString, new TypeToken<List<Map<String, T>>>() {
+            }.getType());
+        }
+        return list;
+    }
+
+    /**
+     * json转成map
+     *
+     * @param gsonString json字符串
+     * @return Map<String, T>
+     */
+    public static <T> Map<String, T> gsonToMaps(String gsonString) {
+        Map<String, T> map = null;
+        if (gson != null) {
+            map = gson.fromJson(gsonString, new TypeToken<Map<String, T>>() {
+            }.getType());
+        }
+        return map;
+    }
 }
