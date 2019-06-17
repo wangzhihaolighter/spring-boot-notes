a70fc8a65a6e78187bf9910a69f834cc672307ba
wangzhihao <1984800194wzh@gmail.com>
2019-06-17 16:42:35 +0800

    更新

--- spring-boot-example/A.hello/hello-world/src/main/java/com/example/HelloWorldApplication.java
+++ spring-boot-example/A.hello/hello-world/src/main/java/com/example/HelloWorldApplication.java
@@ -1,4 +1,4 @@
-package com.example.hello;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class HelloWorldApplication {
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
     }
-
 }
--- spring-boot-example/A.hello/hello-world/src/main/java/com/example/controller/SimpleController.java
+++ spring-boot-example/A.hello/hello-world/src/main/java/com/example/controller/SimpleController.java
@@ -1,10 +1,11 @@
-package com.example.hello.controller;
+package com.example.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleController {
+
     @GetMapping("/")
     public String sayHello() {
         return "Hello,World!";
