4bd866537c2dcf85be69981eb8ead6d8b720a26e
wangzhihao <1984800194wzh@gmail.com>
2019-09-06 17:53:41 +0800

    Spring Boot features - Logging

---
+++ spring-boot-example/B.features/logging/src/main/java/com/example/config/SpringFilterConfig.java
@@ -0,0 +1,10 @@
+package com.example.config;
+
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ServletComponentScan
+public class SpringFilterConfig {
+
+}
---
+++ spring-boot-example/B.features/logging/src/main/java/com/example/controller/SimpleController.java
@@ -0,0 +1,15 @@
+package com.example.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class SimpleController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello,World!";
+    }
+}
