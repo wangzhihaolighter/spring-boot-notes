b3dc33e290dc7aa52d2febd9ac21e200b7e77734
wzh <1984800194wzh@gmail.com>
2020-08-25 14:54:39 +0800

    Spring Boot features - Logging

--- spring-boot-example/B.features/logging/src/main/java/com/example/filter/WebLogFilter.java
+++ spring-boot-example/B.features/logging/src/main/java/com/example/filter/WebLogFilter.java
@@ -1,20 +1,11 @@
 package com.example.filter;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Map;
 
 @Slf4j
 @Component
@@ -32,24 +23,14 @@ public class WebLogFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         log.info(FILTER_NAME + " -> doFilter start : before servlet call service");
 
-        //记录用户访问行为
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
         long startTime = System.currentTimeMillis();
-        log.info("[WEB LOG] URL : " + request.getRequestURL().toString());
-        log.info("[WEB LOG] HTTP_METHOD : " + request.getMethod());
-        log.info("[WEB LOG] IP : " + request.getRemoteAddr());
-        StringBuilder args = new StringBuilder();
-        Map<String, String[]> parameterMap = request.getParameterMap();
-        for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
-            args.append(entry.getKey()).append(":").append(Arrays.toString(entry.getValue())).append(";");
-        }
-        log.info("[WEB LOG] ARGS : " + args);
 
         //doFilter...
         filterChain.doFilter(servletRequest, servletResponse);
 
         long endTime = System.currentTimeMillis();
-        log.info("[WEB LOG] SPEND TIME (ms) : " + (endTime - startTime));
+
+        log.info("[WEB LOG] SPEND TIME : {} ms", (endTime - startTime));
 
         log.info(FILTER_NAME + " -> doFilter end : after servlet call service");
     }
