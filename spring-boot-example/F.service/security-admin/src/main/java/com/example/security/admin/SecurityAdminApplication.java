27a5e3ff49fcd97f1c1a780b98d0ea23dfb75586
wangzhihao <1984800194@qq.com>
2020-12-09 23:56:46 +0800

    service demo - simple security admin

---
+++ spring-boot-example/F.service/security-admin/src/main/java/com/example/security/admin/SecurityAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.security.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAdminApplication.class, args);
+	}
+
+}
---
+++ spring-boot-example/F.service/security-admin/src/main/java/com/example/security/admin/config/cache/CacheConfig.java
@@ -0,0 +1,10 @@
+package com.example.security.admin.config.cache;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+
+}
