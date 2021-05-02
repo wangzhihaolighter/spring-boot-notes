af6a908d4f129a501d9e45873dabaeea31745b96
wangzhihao <1984800194wzh@gmail.com>
2021-05-02 14:54:54 +0800

    update pom.xml

--- spring-boot-example/E.tools/kaptcha/src/main/java/com/example/kaptcha/service/AuthorizationService.java
+++ spring-boot-example/E.tools/kaptcha/src/main/java/com/example/kaptcha/service/AuthorizationService.java
@@ -2,16 +2,18 @@ package com.example.kaptcha.service;
 
 import com.example.kaptcha.dao.UserRepository;
 import com.example.kaptcha.entity.User;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
-@AllArgsConstructor
 @Service
 public class AuthorizationService {
 
     private final UserRepository userRepository;
+
+    public AuthorizationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public User findUser(String username, String password) {
         return userRepository.findUsersByUsernameAndPassword(username, password);
