dd646d47ee1745e05ee28babfa47e6a02f378068
wangzhihao <1984800194wzh@gmail.com>
2021-06-05 11:15:48 +0800

    remove

--- spring-boot-example/F.services/file-manager/src/main/java/com/example/file/manager/dao/FileInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.file.manager.dao;
-
-import com.example.file.manager.entity.FileInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileInfoRepository extends JpaRepository<FileInfo, Long> {
-
-}
