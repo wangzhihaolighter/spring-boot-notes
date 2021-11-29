2ca55610970e362cd18c37557eae95722d35688e
wangzhihao <1984800194wzh@gmail.com>
2021-11-29 23:24:40 +0800

    feature example: auto-configuration

--- spring-boot-example/B.features/auto-configuration/src/test/java/com/example/WrapAutoConfigurationApplicationTests.java
+++ spring-boot-example/B.features/auto-configuration/src/test/java/com/example/WrapAutoConfigurationApplicationTests.java
@@ -1,30 +1,28 @@
 package com.example;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.example.configuration.WrapAutoConfiguration;
 import com.example.service.WrapService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-/**
- * 测试自动化配置
- */
-@RunWith(SpringRunner.class)
+/** 测试自动化配置 */
 @SpringBootTest
 public class WrapAutoConfigurationApplicationTests {
-    private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
-            .withConfiguration(AutoConfigurations.of(WrapAutoConfiguration.class));
+  private final ApplicationContextRunner contextRunner =
+      new ApplicationContextRunner()
+          .withConfiguration(AutoConfigurations.of(WrapAutoConfiguration.class));
 
-    @Test
-    public void serviceNameCanBeConfigured() {
-        this.contextRunner.withPropertyValues("wrap.enabled=true").run((context) -> {
-            assertThat(context).hasSingleBean(WrapService.class);
-        });
-    }
-
+  @Test
+  public void serviceNameCanBeConfigured() {
+    this.contextRunner
+        .withPropertyValues("wrap.enabled=true")
+        .run(
+            (context) -> {
+              assertThat(context).hasSingleBean(WrapService.class);
+            });
+  }
 }
