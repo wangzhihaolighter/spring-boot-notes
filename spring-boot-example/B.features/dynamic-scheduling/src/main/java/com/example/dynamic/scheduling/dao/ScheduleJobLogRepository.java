c9f57b4c7ad66869b9d0a77b27a6d735d6bb0d0c
wangzhihao <1984800194wzh@gmail.com>
2021-12-12 22:46:32 +0800

    clean

--- spring-boot-example/B.features/dynamic-scheduling/src/main/java/com/example/dynamic/scheduling/dao/ScheduleJobLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.dynamic.scheduling.dao;
-
-import com.example.dynamic.scheduling.entity.ScheduleJobLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScheduleJobLogRepository extends JpaRepository<ScheduleJobLog, Long> {
-}
--- spring-boot-example/B.features/dynamic-scheduling/src/main/java/com/example/dynamic/scheduling/dao/ScheduleJobRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.dynamic.scheduling.dao;
-
-import com.example.dynamic.scheduling.entity.ScheduleJob;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScheduleJobRepository extends JpaRepository<ScheduleJob, Long> {
-}
