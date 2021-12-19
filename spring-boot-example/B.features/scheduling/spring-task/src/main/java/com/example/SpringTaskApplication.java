13b9ce5f2ba91d3810fd9b4908945ec9738cc758
wangzhihao <1984800194wzh@gmail.com>
2021-12-19 13:15:52 +0800

    feature example: scheduling - 定时任务调度

--- spring-boot-example/B.features/scheduling/spring-task/src/main/java/com/example/SpringTaskApplication.java
+++ spring-boot-example/B.features/scheduling/spring-task/src/main/java/com/example/SpringTaskApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SchedulingApplication {
+public class SpringTaskApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(SchedulingApplication.class, args);
+    SpringApplication.run(SpringTaskApplication.class, args);
   }
 }
